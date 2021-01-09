4d06c324a56147c0e8a0940be98d0714d26d5bc4
phelucko <1059674+phelucko@users.noreply.github.com>
2021-01-09 13:50:33 +0800

    minor change

--- config.h
+++ config.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11},*/
-	{"",	"pacpackages",	0,	8},
+	/* {"",	"pacpackages",	0,	8}, */
 	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
